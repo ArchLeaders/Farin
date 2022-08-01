61ac439ae72f29d6d3b49d3e45c1112d30b29684
ArchLeaders <archparliament@gmail.com>
2022-07-31 21:00:02 -0700

    `FRT:` Create MD5 hash extension

---
+++ tools/FerinResponceTool/Extensions/HashExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace System.Security.Cryptography
+{
+    internal static class HashExtension
+    {
+        public static string Md5Hash(this string value)
+        {
+            using MD5 md5 = MD5.Create();
+            return Convert.ToBase64String(md5.ComputeHash(Encoding.UTF8.GetBytes(value)));
+        }
+    }
+}
