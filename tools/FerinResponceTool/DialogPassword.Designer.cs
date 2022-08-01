e2e39b5ec917c31ad28ff8cc32ef2c44868474bb
ArchLeaders <archparliament@gmail.com>
2022-07-31 21:00:37 -0700

    `FRT:` Password dialog

---
+++ tools/FerinResponceTool/DialogPassword.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace FarinResponceTool
+{
+    partial class DialogPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DialogPassword));
+            this.BtnOK = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnOK
+            // 
+            this.BtnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnOK.Location = new System.Drawing.Point(254, 78);
+            this.BtnOK.Name = "BtnOK";
+            this.BtnOK.Size = new System.Drawing.Size(112, 34);
+            this.BtnOK.TabIndex = 1;
+            this.BtnOK.Text = "Enter";
+            this.BtnOK.UseVisualStyleBackColor = true;
+            this.BtnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnCancel.Location = new System.Drawing.Point(136, 78);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(112, 34);
+            this.BtnCancel.TabIndex = 2;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(12, 12);
+            this.PasswordBox.MaxLength = 8;
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.PasswordChar = '⋄';
+            this.PasswordBox.PlaceholderText = "Password";
+            this.PasswordBox.Size = new System.Drawing.Size(354, 31);
+            this.PasswordBox.TabIndex = 0;
+            this.PasswordBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.PasswordBox_PreviewKeyDown);
+            // 
+            // GetPassword
+            // 
+            this.AcceptButton = this.BtnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.BtnCancel;
+            this.ClientSize = new System.Drawing.Size(378, 124);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GetPassword";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Enter Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button BtnOK;
+        private Button BtnCancel;
+        private TextBox PasswordBox;
+    }
+}
---
+++ tools/FerinResponceTool/DialogPassword.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FarinResponceTool
+{
+    public partial class DialogPassword : Form
+    {
+        public string? Password { get; private set; } = null;
+        public DialogPassword() => InitializeComponent();
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            Password = PasswordBox.Text;
+            Close();
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            Password = null;
+            Close();
+        }
+
+        private void PasswordBox_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) {
+                BtnOK_Click(null!, null!);
+            }
+        }
+    }
+}
