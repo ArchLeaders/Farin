6d37f840a135f9f4531df03eccaa88d25c61ed0f
ArchLeaders <archparliament@gmail.com>
2022-08-01 11:41:59 -0700

    Remove old HC rolemenu

--- src/cogs/events/on_react.py
+++ src/cogs/events/on_react.py
@@ -27,27 +27,6 @@ class OnReactCog(commands.Cog):
                 elif emoji.name == "❎":
                     await message.delete()
 
-        for message_id, roles in HC_ROLEMENUS.items():
-            if message_id == payload.message_id:
-                role = channel.guild.get_role(roles[emoji.name])
-                if role not in member.roles:
-                    await member.add_roles(role)
-                    return
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_remove(self, payload) -> None:
-
-        channel: TextChannel = await BOT.fetch_channel(payload.channel_id)
-        member: Member = channel.guild.get_member(payload.user_id)
-        emoji = payload.emoji
-
-        for message_id, roles in HC_ROLEMENUS.items():
-            if message_id == payload.message_id:
-                role = channel.guild.get_role(roles[emoji.name])
-                if role in member.roles:
-                    await member.remove_roles(role)
-                    return
-
 
 def setup(bot: commands.Bot):
     bot.add_cog(OnReactCog(bot))
